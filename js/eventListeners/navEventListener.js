a1a8ecf7ea6bc0b617402f3d986df48041345641
DLee1993 <Dai180293@gmail.com>
2021-09-11 14:23:19 +0100

    first commit

--- js/eventListeners/navEventListener.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburger"); 
-const menu = document.querySelector(".nav-list"); 
-
-hamburger.addEventListener("click", () => {
-    menu.classList.toggle("openMenu"); 
-}); 
