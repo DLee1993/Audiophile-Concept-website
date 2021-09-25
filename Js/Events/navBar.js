53d110bf6358ed6f792f9228a4351f305dd6cf41
DLee1993 <Dai180293@gmail.com>
2021-09-25 06:22:33 +0100

    test mobile nav

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -1,11 +1,19 @@
-const btn = document.querySelector(".open-menu"); 
+const openbtn = document.querySelector(".open-menu"); 
 const menu = document.querySelector(".mobile-navbar");
+const closebtn = document.querySelector(".close-menu"); 
+const compName = document.querySelector(".comp-name"); 
 
-btn.addEventListener("click", () => { 
-    if(menu.classList.contains("navbar-open")){
-        menu.classList.remove("navbar-open")
-    } else {
-        menu.classList.add("navbar-open")
-        btn.style.display = "none";
-    }
+openbtn.addEventListener("click", () => { 
+    menu.classList.add("navbar-open")
+    openbtn.style.display = "none";
+    compName.style.display = "block"
 })
+
+closebtn.addEventListener("click", () => {
+    menu.classList.remove("navbar-open")
+    openbtn.style.display = "flex";
+    compName.style.display = "none"
+})
+
+
+//https://youtu.be/2btuhCYf7zw?t=274
