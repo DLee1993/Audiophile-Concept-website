f4241d20142a4b750cad621a93787d683b8314cd
DLee1993 <Dai180293@gmail.com>
2021-09-27 15:37:21 +0100

    removed the company title for menu, removed all margin-top for the link list

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -1,19 +1,16 @@
 const openbtn = document.querySelector(".open-menu"); 
 const menu = document.querySelector(".mobile-navbar");
 const closebtn = document.querySelector(".close-menu"); 
-const compName = document.querySelector(".comp-name"); 
 
 openbtn.addEventListener("click", () => { 
     menu.classList.add("navbar-open")
     openbtn.style.display = "none";
-    compName.style.display = "block"
     document.body.style.overflow = "hidden"
 })
 
 closebtn.addEventListener("click", () => {
     menu.classList.remove("navbar-open")
     openbtn.style.display = "flex";
-    compName.style.display = "none"
 })
 
 
