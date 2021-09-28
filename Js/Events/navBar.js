03c442798102b4d953a157c84a45928cf965c3b0
DLee1993 <Dai180293@gmail.com>
2021-09-28 09:26:35 +0100

    category links styled and responsive

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -1,17 +1,2 @@
-const openbtn = document.querySelector(".open-menu"); 
-const menu = document.querySelector(".mobile-navbar");
-const closebtn = document.querySelector(".close-menu"); 
-
-openbtn.addEventListener("click", () => { 
-    menu.classList.add("navbar-open")
-    openbtn.style.display = "none";
-    document.body.style.overflow = "hidden"
-})
-
-closebtn.addEventListener("click", () => {
-    menu.classList.remove("navbar-open")
-    openbtn.style.display = "flex";
-})
-
 
 //https://youtu.be/2btuhCYf7zw?t=274
