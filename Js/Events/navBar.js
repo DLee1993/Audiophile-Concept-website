e26dabcefc7da98bc54ed29abd10d75911b75f23
DLee1993 <Dai180293@gmail.com>
2021-09-30 19:29:04 +0100

    added the sub menu for the mobile navbar and added the js functionality to the opening/closing of the sub menu

---
+++ Js/Events/navBar.js
@@ -0,0 +1,15 @@
+const categories = document.getElementById("category-link"); 
+const subList = document.querySelector(".product-list"); 
+const main = document.querySelector(".main-content")
+
+categories.addEventListener("click", () => {
+    if(subList.classList.contains("open-subList")){
+        subList.classList.remove("open-subList")
+    } else {
+        subList.classList.add("open-subList")
+    }
+});
+
+main.addEventListener("click", () => {
+    subList.classList.remove("open-subList")
+})
