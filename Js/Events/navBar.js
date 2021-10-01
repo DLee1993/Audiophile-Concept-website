b3a72ba8e182a56248d0440e464569cffca88102
DLee1993 <Dai180293@gmail.com>
2021-10-01 16:22:05 +0100

    added the functionality for the opening/closing of the sublist in mobile vp

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -1,13 +1,18 @@
 const categories = document.getElementById("category-link"); 
 const subList = document.querySelector(".product-list"); 
 const main = document.querySelector(".main-content"); 
-const navbar = document.querySelector(".navbar"); 
-const navbarNav = document.querySelector(".navbar-nav"); 
 
 categories.addEventListener("click", () => {
     if(subList.classList.contains("open-subList")){
         subList.classList.remove("open-subList")
+        categories.style.backgroundColor = "#D87D4A"
     } else {
         subList.classList.add("open-subList")
+        categories.style.backgroundColor = "#8a5130"
     }
+});
+
+main.addEventListener("click", () => {
+    subList.classList.remove("open-subList")
+    categories.style.backgroundColor = "#D87D4A"
 });
