aab144077fc4b4c4741fb16299c366b253370433
DLee1993 <Dai180293@gmail.com>
2021-10-01 19:09:44 +0100

    added desktop navbar

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -5,14 +5,11 @@ const main = document.querySelector(".main-content");
 categories.addEventListener("click", () => {
     if(subList.classList.contains("open-subList")){
         subList.classList.remove("open-subList")
-        categories.style.backgroundColor = "#D87D4A"
     } else {
         subList.classList.add("open-subList")
-        categories.style.backgroundColor = "#8a5130"
     }
 });
 
 main.addEventListener("click", () => {
     subList.classList.remove("open-subList")
-    categories.style.backgroundColor = "#D87D4A"
 });
