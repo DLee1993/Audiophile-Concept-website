2cf934228dce1d9c15344b65a747a13b3a77a1cb
DLee1993 <Dai180293@gmail.com>
2021-10-03 00:42:29 +0100

    minor changes to colors on click of categories btn

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -1,15 +1,23 @@
-const categories = document.getElementById("category-link"); 
-const subList = document.querySelector(".product-list"); 
-const main = document.querySelector(".main-content"); 
+const categories = document.getElementById("category-link");
+const categoriesSVG = document.querySelector(".products-svg");
+const categoriesText = document.querySelector(".catLink-text");
+const subList = document.querySelector(".product-list");
+const main = document.querySelector(".main-content");
 
 categories.addEventListener("click", () => {
-    if(subList.classList.contains("open-subList")){
-        subList.classList.remove("open-subList")
+    if (subList.classList.contains("open-subList")) {
+        subList.classList.remove("open-subList");
+        categoriesSVG.style.fill = "#fff";
+        categoriesText.style.color = "#F1F1F1";
     } else {
-        subList.classList.add("open-subList")
+        subList.classList.add("open-subList");
+        categoriesSVG.style.fill = "#4c4c4c";
+        categoriesText.style.color = "#4c4c4c";
     }
 });
 
 main.addEventListener("click", () => {
-    subList.classList.remove("open-subList")
+    subList.classList.remove("open-subList");
+    categoriesSVG.style.fill = "#fff";
+    categoriesText.style.color = "#F1F1F1";
 });
