081d15fc434bd2665348d7acebb24248d69a0e11
DLee1993 <Dai180293@gmail.com>
2021-10-05 10:27:56 +0100

    added width of 100% to both nav items and product items in both menus

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -1,10 +1,13 @@
 const productsBtn = document.querySelector(".subMenu-btn"), 
+    menu = document.querySelector(".mobile-navigation"),
     subMenu = document.querySelector(".mobile-subMenu")
 
 productsBtn.addEventListener("click", () => {
     if(subMenu.classList.contains("menu-open")){
         subMenu.classList.remove("menu-open")
+        menu.style.borderTop = "none"
     } else {
         subMenu.classList.add("menu-open")
+        menu.style.borderTop = "2px solid #F1F1F1"
     }
 })
