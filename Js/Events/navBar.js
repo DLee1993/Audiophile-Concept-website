44d84060984402cb0d55b43e3c73ce966d100bb4
DLee1993 <Dai180293@gmail.com>
2021-10-06 22:23:58 +0100

    navigation markup added, also added repsonsive design to navigation

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -1,42 +1,17 @@
-const productsBtn = document.querySelector(".subMenu-btn"),
-    menu = document.querySelector(".mobile-navigation"),
-    subMenu = document.querySelector(".mobile-subMenu"),
-    tabletMenu = document.querySelector(".tablet-desktop-navigation"),
-    tabletOpenBtn = document.querySelector(".open-btn"),
-    tabletCloseBtn = document.querySelector(".close-btn");
+const test = document.querySelector(".nav"), 
+menu = document.querySelector(".navbar"), 
+mainContent = document.querySelector(".main-content")
 
-productsBtn.addEventListener("click", () => {
-    if (subMenu.classList.contains("mobile-menu-open")) {
-        subMenu.classList.remove("mobile-menu-open");
-        productsBtn.style.color = "#F1F1F1";
-        menu.style.borderTop = "none";
-    } else {
-        subMenu.classList.add("mobile-menu-open");
-        productsBtn.style.color = "#4c4c4c";
-        menu.style.borderTop = "2px solid #F1F1F1";
+test.addEventListener("click", () => {
+    if(menu.classList.contains("nav-open")){
+        menu.classList.remove("nav-open")
+    } else{
+        menu.classList.add("nav-open")
     }
-});
+})
 
-tabletOpenBtn.addEventListener("click", () => {
-    tabletMenu.classList.add("tablet-menu-open");
-    tabletOpenBtn.style.display = "none";
-    tabletCloseBtn.style.display = "block";
-});
-
-tabletCloseBtn.addEventListener("click", () => {
-    tabletMenu.classList.remove("tablet-menu-open");
-    tabletOpenBtn.style.display = "block";
-    tabletCloseBtn.style.display = "none";
-});
-
-window.addEventListener("resize", () => {
-    if (window.innerWidth > 600) {
-        subMenu.classList.remove("mobile-menu-open");
-        productsBtn.style.color = "#F1F1F1";
+mainContent.addEventListener("click", () => {
+    if(menu.classList.contains("nav-open")){
+        menu.classList.remove("nav-open")
     }
-    if (window.innerWidth > 900) {
-        tabletMenu.classList.remove("tablet-menu-open");
-        tabletOpenBtn.style.display = "block";
-        tabletCloseBtn.style.display = "none";
-    }
-});
+})
