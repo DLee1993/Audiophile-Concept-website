d9455ebc55b746795539f102ca022ea3a400b36c
DLee1993 <Dai180293@gmail.com>
2021-10-08 11:31:13 +0100

    changed the width of the hamburger menu

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -5,8 +5,10 @@ mainContent = document.querySelector(".main-content")
 hamburger.addEventListener("click", () => {
     if(menu.classList.contains("nav-open")){
         menu.classList.remove("nav-open")
+        hamburger.style.fill = "#fff"
     } else{
         menu.classList.add("nav-open")
+        hamburger.style.fill = "#D87D4A"
     }
 })
 
