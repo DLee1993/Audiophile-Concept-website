6cd4e6ba516ed4f45c5cc0df06c9a7c4fc995bf1
DLee1993 <Dai180293@gmail.com>
2021-10-27 05:44:48 +0100

    add a border bottom to nav, also added a small transition for the index header, change from transparent to black on scroll

--- Js/Events/navBar.js
+++ Js/Events/navBar.js
@@ -1,23 +1,38 @@
-const hamburger = document.querySelector(".hamburger-icon"), 
-menu = document.querySelector(".mobile-nav-list"), 
-mainContent = document.querySelector(".main-content")
+const hamburger = document.querySelector(".hamburger-icon"),
+    menu = document.querySelector(".mobile-nav-list"),
+    mainContent = document.querySelector(".main-content"),
+    nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => {
-    if(menu.classList.contains("nav-open")){
-        menu.classList.remove("nav-open")
-        hamburger.style.fill = "#fff"
-        document.body.style.overflow = "auto"
-    } else{
-        menu.classList.add("nav-open")
-        hamburger.style.fill = "#D87D4A"
-        document.body.style.overflow = "hidden"
+    if (menu.classList.contains("nav-open")) {
+        menu.classList.remove("nav-open");
+        hamburger.style.fill = "#fff";
+        document.body.style.overflow = "auto";
+    } else {
+        menu.classList.add("nav-open");
+        hamburger.style.fill = "#D87D4A";
+        document.body.style.overflow = "hidden";
     }
-})
+});
 
 mainContent.addEventListener("click", () => {
-    if(menu.classList.contains("nav-open")){
-        menu.classList.remove("nav-open")
-        hamburger.style.fill = "#fff"
-        document.body.style.overflow = "auto"
+    if (menu.classList.contains("nav-open")) {
+        menu.classList.remove("nav-open");
+        hamburger.style.fill = "#fff";
+        document.body.style.overflow = "auto";
     }
-})
+});
+
+if (document.URL.includes("index.html")) {
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            nav.style.backgroundColor = "black";
+            nav.style.transition = "background-color .3s ease"
+        } else {
+            nav.style.backgroundColor = "transparent";
+        }
+    });
+} else {
+    nav.style.backgroundColor = "black";
+    nav.style.transition = "none"
+}
