a00a4351ea2ea2d3649ad66752461da19ac2f1ef
DLee1993 <Dai180293@gmail.com>
2021-11-08 11:05:00 +0000

    minor margin changes for the form title and the order confirmed modal

--- Js/Events/checkout.js
+++ Js/Events/checkout.js
@@ -1,4 +1,4 @@
 //* The below code is strictly to change the background color of the checkout page, this is done according to design spec*/
-if(document.URL.includes("checkout.html")){
-    document.body.style.backgroundColor = "#F1F1F1"
+if (document.URL.includes("checkout.html")) {
+    document.body.style.backgroundColor = "#F1F1F1";
 }
